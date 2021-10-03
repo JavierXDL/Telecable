98ca07184a8de2e5d847ba3b8f7815415fd902a0
JavierXDL <yairjavier309@gmail.com>
2021-10-03 17:06:27 -0500

    Canales modificado

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 
-
-
-
 //metodo para entrar a todos los controladores
 Route::resource('/', InicioController::class);
 Route::get('listaCanales', [InicioController::class, 'canales'])->name('vistaCanalaes');
